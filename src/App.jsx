52f856c9c7c7a05f8b8db6fafd1ea68019f8b18f
Guneet Singh <itsguneetsingh@gmail.com>
2023-12-22 21:17:41 +0530

    Smooth Scrolling is FINALLLLLLY working

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Background from './Components/Background'
 import Foreground from './Components/Foreground'
 import Mask from './Components/mask'
 import Skills from './Components/Skills.jsx'
-import { ScrollContainer, ScrollPage} from "react-scroll-motion";
-
+// import { ScrollContainer, ScrollPage} from "react-scroll-motion";
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 const App = () => {
+
+  const containerRef = useRef(null)
+
   return (
-    <ScrollContainer>
-      <div id='root' className='w-full h-screen bg-zinc-800 object-cover relative'>
+    // <ScrollContainer>
+        <LocomotiveScrollProvider
+      options={
+        {
+          smooth: true,
+          // ... all available Locomotive Scroll instance options 
+        }
+      }
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      containerRef={containerRef}
+    >
+    <main data-scroll-container ref={containerRef}>
+      <div data-scroll-section className='w-full h-screen bg-zinc-800 object-cover relative'>
         <Background/>
         <Foreground/>
       </div>
-      <div id='root' className='w-full min-h-screen bg-zinc-800 object-cover relative'>
+      <div data-scroll-section className='w-full h-screen bg-zinc-800 object-cover relative'>
         <Mask/>
       </div>
-      <ScrollPage>
+      {/* <ScrollPage> */}
+      <div data-scroll-section>
         <Skills/>
-      </ScrollPage>
-    </ScrollContainer>
+      </div>
+      </main>
+      </LocomotiveScrollProvider>
+      // </ScrollPage>
+    // </ScrollContainer>
   )
 }
 
